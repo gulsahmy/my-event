fec8af62f0d96fef268ce88ffc482a0014cde8b1
gulsah <gmutluayyigit@gmail.com>
2024-04-05 15:09:48 +0300

    cut-copy-paste

--- src/components/keyboardClipboardEvents/KeyboardClipboard.jsx
+++ src/components/keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -1,7 +1,62 @@
 
+import {useState} from "react"
+
 const KeyboardClipboard = () => {
+
+    const [inputData, setInputData] = useState("")
+
+   const handleKeyDown = (e) => {
+    console.log(e.keyCode)
+    if (e.keyCode > 47 && e.keyCode < 58) {
+        alert("a number pressed")
+        e.preventDefault()
+    }
+   }
+   const handleCopyArea = (e) => {
+    alert("Kopyalama yapılamaz")
+    e.preventDefault()
+   }
+
+   const handleCutArea = (e) => {
+    alert("Bu metin kesilemez")
+    e.preventDefault()
+   }
+
+   const handlePasteArea = (e) => {
+    e.target.style.fontFamily = "tahoma"
+    e.target.style.fontSize = "1.4rem"
+    e.target.style.color = "red"
+   }
+
     return (
-        <div>KeyboardClipboard</div>
+        <div className="container mt-4">
+         <h2>Keyboard Events</h2>   
+
+         <input 
+         type="text" 
+         classname="form-control" 
+         onChange={(e) => setInputData(e.target.value.toLocaleUpperCase())} 
+         value={inputData}
+         onKeyDown={handleKeyDown}
+         />
+
+         <div className="mt-3">
+            <p>{inputData.toLocaleUpperCase()}</p>
+
+         </div>
+
+         <div>
+            <textarea classname="form-control"
+            name="textarea"
+            id="area"
+            cols="30"
+            rows="10"
+            onCopy={handleCopyArea}
+            onCut={handleCutArea}
+            onPaste={handlePasteArea}
+            ></textarea>
+         </div>          
+        </div>
     )
 }
 
